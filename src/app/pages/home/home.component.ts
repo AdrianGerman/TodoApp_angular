edccf2063c7f2239b29370c62107b9ba8cc2321e
AdrianGerman <adrian.german1019@gmail.com>
2024-02-22 11:06:16 -0600

    Control directives

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], //mandamos los imports a nuestro archivo html
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  tasks = signal([
+    'Tomarme un mate',
+    'Crear mi primer proyecto en Angular',
+    'Hacer mis sprites',
+    'Subir mis cambios a GitLab',
+  ]);
 }
